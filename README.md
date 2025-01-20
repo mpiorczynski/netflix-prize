39a59d529a72e4d10df4be9b068988b901d9174a
Mikolaj Piorczynski <m.piorczynski13@gmail.com>
2025-01-20 23:47:21 +0100

    Add args to scripts

--- README.md
+++ README.md
@@ -8,5 +8,26 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 pre-commit install
 ```
 
+## Data
+The data is available at [Netflix Prize](https://www.kaggle.com/netflix-inc/netflix-prize-data).
+
+## Usage
+
+### Training
+Change the configuration file in `configs/` to the desired model and hyperparameters. Then run the following command:
+```bash
+python netflix_prize/train.py configs/centered_knn.yaml
+```
+
+### Recommendations
+```bash
+python netflix_prize/recommend_movie.py model_path="checkpoints/centered_knn/model.pkl" recommend.movie_title="Toy Story"
+```
+
+```bash
+python netflix_prize/recommend_user.py model_path="checkpoints/centered_knn/model.pkl" recommend.user_id=1377724
+```
+
+
 ## References
 [1] https://en.wikipedia.org/wiki/Netflix_Prize
--- netflix_prize/recommend_movie.py
+++ netflix_prize/recommend_movie.py
@@ -1,46 +1,56 @@
 import numpy as np
+from omegaconf import DictConfig
 
 from netflix_prize.data import load_movies
-from netflix_prize.utils import format_number, load_model
+from netflix_prize.utils import format_number, load_config, load_model
 
-movie_title = "Batman Begins"
-model_path = "checkpoints/centered_knn/model.pkl"
-top_k = 10
-movies_path = "data/movie_titles_fixed.csv"
 
-movies_df = load_movies(movies_path)
-print(f"Number of movies in the database: {format_number(movies_df.shape[0])}")
-print(f"The oldest movie: {int(movies_df['year'].min())}")
-print(f"The newest movie: {int(movies_df['year'].max())}")
+@load_config("configs/default.yaml")
+def main(cfg: DictConfig):
+    print(f"Looking for recommendations for the {cfg.recommend.movie_title}")
 
-movies_filtered = movies_df[movies_df["title"] == movie_title].to_dict("records")
+    print("Loading available movies...")
+    movies_df = load_movies(cfg.data.movies_file)
+    print(f"Number of movies in the database: {format_number(movies_df.shape[0])}")
 
-if len(movies_filtered) == 0:
-    print(f"Movie with title {movie_title} not found.")
-else:
-    if len(movies_filtered) > 1:
-        print(f"Multiple versions of the {movie_title} found. Selecting the first one.")
-    movie_raw_id = movies_filtered[0]["movie_id"]
-    movie_year = int(movies_filtered[0]["year"])
+    movies_filtered = movies_df[movies_df["title"] == cfg.recommend.movie_title].to_dict(
+        "records"
+    )
 
-movie_raw_id_to_title = movies_df.set_index("movie_id").to_dict("index")
-# add a year to the title
-movie_raw_id_to_title = {k: f"{v['title']} ({int(v['year']) if not np.isnan(v['year']) else '-'})" for k, v in movie_raw_id_to_title.items()}
+    if len(movies_filtered) == 0:
+        print(f"Movie with title {cfg.recommend.movie_title} not found.")
+    else:
+        if len(movies_filtered) > 1:
+            print(
+                f"Multiple versions of the {cfg.recommend.movie_title} found. Selecting the first one."
+            )
+        movie_raw_id = movies_filtered[0]["movie_id"]
+        movie_year = int(movies_filtered[0]["year"])
 
-print(f"Loading model from {model_path}")
-model = load_model(model_path)
+    movie_raw_id_to_title = movies_df.set_index("movie_id").to_dict("index")
+    # add a year to the title
+    movie_raw_id_to_title = {
+        k: f"{v['title']} ({int(v['year']) if not np.isnan(v['year']) else '-'})"
+        for k, v in movie_raw_id_to_title.items()
+    }
 
-movie_inner_id = model.trainset.to_inner_iid(movie_raw_id)
+    print(f"Loading model from {cfg.model_path}")
+    model = load_model(cfg.model_path)
 
-print(f"Getting {top_k} similar movies to {movie_title} ({movie_year})...")
-similar_movies = model.get_neighbors(movie_inner_id, k=top_k)
-similar_movies = (
-    model.trainset.to_raw_iid(inner_id) for inner_id in similar_movies
-)
-similar_movies = (movie_raw_id_to_title[raw_id] for raw_id in similar_movies)
+    print("Finding recommendations...")
+    movie_inner_id = model.trainset.to_inner_iid(movie_raw_id)
+    similar_movies = model.get_neighbors(
+        movie_inner_id, k=cfg.recommend.num_recommendations
+    )
+    similar_movies = (model.trainset.to_raw_iid(inner_id) for inner_id in similar_movies)
+    similar_movies = (movie_raw_id_to_title[raw_id] for raw_id in similar_movies)
 
-print()
-print(f"Recommended after watching {movie_title} ({movie_year}):")
-print("-" * 40)
-for i, movie in enumerate(similar_movies):
-    print(f"{i+1}. ", movie)
+    print()
+    print(f"Recommended after watching {cfg.recommend.movie_title} ({movie_year}):")
+    print("-" * 40)
+    for i, movie in enumerate(similar_movies):
+        print(f"{i+1}. ", movie)
+
+
+if __name__ == "__main__":
+    main()
